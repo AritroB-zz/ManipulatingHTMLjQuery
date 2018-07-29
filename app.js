e4ed8e13b1fbf6797067418a7cc31083f2329fcd
Aritro Basu <abasuusa2010@gmail.com>
2018-07-29 17:59:28 -0500

    Finished up the lab

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-let $button = $("<button>1. Click Me!</button>");
+/* let $button = $("<button>1. Click Me!</button>");
 $button.appendTo("body");
 $button.click(function () {
     alert("Messi is the GOAT!")
-});
+});*/
 
+/* OBJECTIVE 2
 let $button = $("#button");
 $button.click( function () {
     let message = $("#text-box").val();
     alert(message);
 });
+
+*/
+
+/* OBJECTIVE 3 
+let $div = $("#div");
+$div.hover(function() {
+    div.style.backgroundColor = "red";
+}, function() {
+    div.style.backgroundColor = "black";
+});*/
+
+/* OBJECTIVE 4
+let $p = $("#p");
+$p.click( function() {
+    let $allchar = "0123456789ABCDEF";
+    let $randcol = "";
+    for (let i = 0; i < 6; i++) {
+        $randcol += $allchar[Math.floor(Math.random() * 16)];
+    }
+    $($p).css("color", "#" + $randcol);
+});
+*/
+
+ /*OBJECTIVE 5*/
+let $button = $("#button");
+let $div = $("#div");
+$button.click( function() {
+    let $span = $("<span> Aritro</span>");
+    $($span).appendTo($div);
+});
+
